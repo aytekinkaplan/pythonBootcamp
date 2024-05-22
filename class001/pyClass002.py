de6ac011a4c515e67eab8c47c872979fb222f7cf
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-22 23:54:16 +0300

    New Commit

---
+++ class001/pyClass002.py
@@ -0,0 +1,39 @@
+print(24 * 60 * 60)
+
+print(24 * 60 * 60 * 365)
+
+print(24 * 60 * 60 * 365 * 10)
+
+print(24 * 60 * 60 * 365 * 10 * 100)
+
+print(abs(-24 * 60 * 60 * 365 * 10 * 100))
+
+print(abs(-450))
+
+print(abs(450))
+
+print(abs(0))
+
+print(abs(-0))
+
+print(pow(2, 3))
+
+print(pow(3, 3))
+
+print(pow(4, 3))
+
+print(pow(5, 3))
+
+print(pow(6, 3))
+
+print(pow(7, 3))
+
+print(pow(8, 3))
+
+print("The maximum is {}".format(max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
+
+print("The minimum is {}".format(min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
+
+print("The sum is {}".format(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
+
+print("The power is {}".format(pow(10, 3)))

d8665fb60312538033c441a6484b468c87ea9390
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-22 23:26:31 +0300

    New Commit

---
+++ class001/pyClass001.py
@@ -0,0 +1,27 @@
+print("Hello Python!")
+
+string = "Hello World!"
+
+print(string)
+
+
+def add(x, y):
+    return x + y
+
+
+print(add(1, 2))
+
+
+def hello(name):
+    print("Hello, " + name)
+
+
+hello("World")
+
+print(5*5)
+
+print(5**5)
+
+print(5/5)
+
+print(5//5)

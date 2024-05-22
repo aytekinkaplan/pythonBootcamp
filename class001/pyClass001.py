e42c4b69abc83d4dbf7375f31be3272090bf9088
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-22 23:28:20 +0300

    New Commit

--- class001/pyClass001.py
+++ class001/pyClass001.py
@@ -18,10 +18,39 @@ def hello(name):
 
 hello("World")
 
-print(5*5)
+print(5 * 5)
 
-print(5**5)
+print(5 ** 5)
 
-print(5/5)
+print(5 / 5)
 
-print(5//5)
+print(5 // 5)
+
+print(5 % 5)
+
+print(5 + 5)
+
+print(5 - 5)
+
+print(5 < 5)
+
+print(5 > 5)
+
+print(5 == 5)
+
+print(5 != 5)
+
+print(5 <= 5)
+
+print(5 >= 5)
+
+print(5 in [1, 2, 3])
+
+print(5 not in [1, 2, 3])
+
+
+print(5 is 5)
+
+print(5 is not 5)
+
+

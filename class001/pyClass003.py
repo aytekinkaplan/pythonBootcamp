d16021cbf0e8234dd2ff36e5794d60559893cf94
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-23 00:22:20 +0300

    New Commit

--- class001/pyClass003.py
+++ class001/pyClass003.py
@@ -5,4 +5,33 @@ y = 10
 z = x + y
 print(z)
 
+print(type(x))
+print(type(y))
+print(type(z))
 
+x = "Hello"
+y = "World"
+z = x + y
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = True
+y = False
+z = x or y
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))

348a98197d1e21b20e266c83c7477862f4abac6a
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:21 +0300

    New Commit

---
+++ class002/pyClass002.py
@@ -0,0 +1,20 @@
+sum = 0
+for i in range(11):
+    sum += i
+print(sum)
+
+for i in range(1, 11):
+    print(f"5*{i} = {5 * i}")
+
+for i in range(1, 11):
+    print(5 * i)
+
+newSum = 0
+for i in range(1, 11):
+    newSum += i * i
+print(newSum)
+
+newerSum = 0
+for i in range(1, 11):
+    newerSum += i * i * i
+print(newerSum)

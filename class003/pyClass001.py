6211be38895614e6dda9f89b207c849eb593c003
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:15 +0300

    New Commit

---
+++ class003/pyClass001.py
@@ -0,0 +1,26 @@
+def helloWorld():
+    print("Hello World")
+
+
+helloWorld()
+
+
+def hello(name):
+    print("Hello " + name)
+
+
+hello("Aytekin")
+
+
+def add(x, y):
+    print(x + y)
+
+
+add(5, 10)
+
+
+def multiply(x, y):
+    print(x * y)
+
+
+multiply(5, 10)

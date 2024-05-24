9a68defe6ce0ad949f71f4cce8bca5f7c1aae6f2
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:34 +0300

    New Commit

---
+++ class002/pyClass003.py
@@ -0,0 +1,15 @@
+# Calculating the factorial of a number using a for loop
+
+# num = int(input("Enter a number: "))
+#
+# factorial = 1
+#
+# for i in range(1, num + 1):
+#     factorial = factorial * i
+#
+# print("The factorial of", num, "is", factorial)
+factorial = 1
+for i in range(1, 6):
+    factorial = factorial * i
+
+print(factorial)

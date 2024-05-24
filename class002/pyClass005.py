2afece2d53cacceb7d99c4dd3c7ef7e06d4dfcac
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:50 +0300

    New Commit

---
+++ class002/pyClass005.py
@@ -0,0 +1,23 @@
+for i in range(1, 6):
+    print("*****")
+
+for i in range(1, 6):
+    print("*    *")
+
+for i in range(1, 6):
+    print("*    *")
+
+for i in range(1, 6):
+    print("*    *")
+
+for i in range(1, 6):
+    print("*    *")
+
+for i in range(1, 6):
+    print("*****")
+
+for i in range(1, 6):
+    print("*", end="")
+
+for i in range(1, 6):
+    print("Aytekin", end="")

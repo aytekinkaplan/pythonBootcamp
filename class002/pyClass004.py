10f97b5877b72181e57ed7e3cc9d5c64cb9ced9b
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:44 +0300

    New Commit

---
+++ class002/pyClass004.py
@@ -0,0 +1,27 @@
+for i in range(5):
+    for j in range(5):
+        print(i, j)
+for i in range(5):
+    print(i)
+
+for i in range(2, 11):
+    print(i)
+
+for i in range(2, 11, 2):
+    print(i)
+
+for i in range(3, 13, 3):
+    print(i)
+
+for i in range(3, -4, -1):
+    print(i)
+
+sum = 0
+for i in range(1, 101):
+    sum += i
+print(sum)
+
+sum = 0
+for i in range(1, 101, 2):
+    sum += i
+print(sum)

dac358c5fa268ec1b066a866ea6a9b0d4934eed9
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:27 +0300

    New Commit

---
+++ class003/pyClass002.py
@@ -0,0 +1,24 @@
+def cube_of_number(number):
+    result = number * number * number
+    return result
+
+
+print(cube_of_number(5))
+
+print(cube_of_number(10))
+
+
+def product_of_four_numbers(number1, number2, number3, number4):
+    result = number1 * number2 * number3 * number4
+    return result
+
+
+print(product_of_four_numbers(1, 2, 3, 4))
+
+
+def average_of_five_numbers(number1, number2, number3, number4, number5):
+    result = (number1 + number2 + number3 + number4 + number5) / 5
+    return result
+
+
+print(average_of_five_numbers(1, 2, 3, 4, 5))

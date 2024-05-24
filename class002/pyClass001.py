cb1976bda0e8bc33486f3ba4c1c85d1a22a694ff
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-24 07:54:09 +0300

    New Commit

---
+++ class002/pyClass001.py
@@ -0,0 +1,17 @@
+for i in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
+    print(i)
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+for i in range(0, 100, 12):
+    print(i)
+
+for i in range(11):
+    print(i * i)

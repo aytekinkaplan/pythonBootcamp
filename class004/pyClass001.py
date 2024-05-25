0a0c1ebc1bf3fa1822ad949d8ccbf1a6530d5db5
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-25 06:35:35 +0300

    New Commit

---
+++ class004/pyClass001.py
@@ -0,0 +1,70 @@
+# Data Types
+# String
+string = "Hello World"
+print(string)
+print(len(string))
+print(string.split(" "))
+string = string.upper()
+print(string)
+string = string.lower()
+print(string)
+string = string.title()
+print(string)
+string = string.capitalize()
+print(string)
+string = string.replace("World", "Universe")
+print(string)
+string = string.strip()
+print(string)
+# Integer
+number = 10
+print(number)
+print(type(number))
+number = number + 5
+print(number)
+print(type(number))
+
+umber *= 2
+print(number)
+
+number /= 2
+print(number)
+
+number -= 5
+print(number)
+
+number += 5
+print(number)
+
+number %= 3
+print(number)
+
+number //= 3
+print(number)
+
+number **= 3
+print(number)
+
+number &= 3
+print(number)
+
+number |= 3
+print(number)
+
+number ^= 3
+print(number)
+
+number >>= 3
+print(number)
+
+number <<= 3
+print(number)
+
+
+# Float
+
+# Boolean
+# List
+# Dictionary
+# Tuple
+# Set

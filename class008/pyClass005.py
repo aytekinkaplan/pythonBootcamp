c4e460f78b669f1418d47bf2dff8701d5d4fff0b
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-26 07:49:00 +0300

    New Commit

---
+++ class008/pyClass005.py
@@ -0,0 +1,51 @@
+class Planet:
+    pass
+
+
+earth = Planet()
+earth.name = "Earth"
+earth.diameter = 12742
+earth.gravity = 9.8
+print(earth.name)
+print(earth.diameter)
+print(earth.gravity)
+
+venus = Planet()
+venus.name = "Venus"
+venus.diameter = 12104
+venus.gravity = 8.87
+print(venus.name)
+print(venus.diameter)
+print(venus.gravity)
+
+mars = Planet()
+mars.name = "Mars"
+mars.diameter = 6779
+mars.gravity = 3.711
+print(mars.name)
+print(mars.diameter)
+print(mars.gravity)
+
+jupiter = Planet()
+jupiter.name = "Jupiter"
+jupiter.diameter = 142984
+jupiter.gravity = 24.79
+print(jupiter.name)
+print(jupiter.diameter)
+print(jupiter.gravity)
+
+uranus = Planet()
+uranus.name = "Uranus"
+uranus.diameter = 51118
+uranus.gravity = 8.69
+print(uranus.name)
+print(uranus.diameter)
+print(uranus.gravity)
+
+neptune = Planet()
+neptune.name = "Neptune"
+neptune.diameter = 49528
+neptune.gravity = 11.15
+print(neptune.name)
+print(neptune.diameter)
+print(neptune.gravity)

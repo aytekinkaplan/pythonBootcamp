393410774fff8828639a6e736c71c0aad616f8b0
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-26 07:48:46 +0300

    New Commit

---
+++ class008/pyClass004.py
@@ -0,0 +1,38 @@
+class MotorBike:
+    pass
+
+
+bike1 = MotorBike()
+bike2 = MotorBike()
+bike1.mileage = 20
+bike1.color = "blue"
+bike2.mileage = 30
+bike2.color = "red"
+print(bike1.mileage)
+print(bike1.color)
+print(bike2.mileage)
+print(bike2.color)
+
+
+class Book:
+    pass
+
+
+book1 = Book()
+book2 = Book()
+book1.price = 100
+book1.pages = 100
+book1.author = "John Doe"
+book1.title = "My Book"
+book2.price = 200
+book2.pages = 200
+book2.author = "Jane Doe"
+book2.title = "My Second Book"
+print(book1.price)
+print(book1.pages)
+print(book1.author)
+print(book1.title)
+print(book2.price)
+print(book2.pages)
+print(book2.author)
+print(book2.title)

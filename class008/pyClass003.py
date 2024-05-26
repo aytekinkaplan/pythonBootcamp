c978f069b630fd7db04d8471e312cdbc80bdd8b9
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-26 07:48:35 +0300

    New Commit

---
+++ class008/pyClass003.py
@@ -0,0 +1,55 @@
+class Country:
+    pass
+
+
+usa = Country()
+usa.name = "United States of America"
+usa.capital = "Washington, D.C."
+usa.population = 328_200_000
+usa.language = "English"
+usa.currency = "US Dollar"
+
+print(usa.name)
+print(usa.capital)
+print(usa.population)
+print(usa.language)
+print(usa.currency)
+
+france = Country()
+france.name = "France"
+france.capital = "Paris"
+france.population = 66_000_000
+france.language = "French"
+france.currency = "Euro"
+
+print(france.name)
+print(france.capital)
+print(france.population)
+print(france.language)
+print(france.currency)
+
+holland = Country()
+holland.name = "Netherlands"
+holland.capital = "Amsterdam"
+holland.population = 16_000_000
+holland.language = "Dutch"
+holland.currency = "Euro"
+
+print(holland.name)
+print(holland.capital)
+print(holland.population)
+print(holland.language)
+print(holland.currency)
+
+germany = Country()
+germany.name = "Germany"
+germany.capital = "Berlin"
+germany.population = 83_000_000
+germany.language = "German"
+germany.currency = "Euro"
+
+print(germany.name)
+print(germany.capital)
+print(germany.population)
+print(germany.language)
+print(germany.currency)

7307e4e46bb75d4e7437faf9d73d008afc8b15b1
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-26 07:47:56 +0300

    New Commit

---
+++ class008/pyClass001.py
@@ -0,0 +1,26 @@
+class Customer:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+customer = Customer("John", 25)
+customer.display()
+
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print("Name:", self.name)
+        print("Salary:", self.salary)
+
+
+employee = Employee("Jane", 50000)
+employee.display()

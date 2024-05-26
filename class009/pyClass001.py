82853b66727fa8a4904f458926f89d953af7344c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-26 07:48:02 +0300

    New Commit

---
+++ class009/pyClass001.py
@@ -0,0 +1,13 @@
+mark1 = 45
+mark2 = 55
+mark3 = 65
+mark4 = 75
+mark5 = 85
+marks = [mark1, mark2, mark3, mark4, mark5]
+
+total = sum(marks)
+print(total)
+
+average = total / len(marks)
+print(average)
+

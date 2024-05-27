3e73c965127996747b929f1d3e165fe06e4e46ba
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-28 01:36:21 +0300

    New Commit

---
+++ class018/pyClass001.py
@@ -0,0 +1,19 @@
+numbers_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(numbers_set)
+string_set = {"a", "b", "c", "d", "e", "f", "g"}
+print(string_set)
+
+empty_set = set()
+print(empty_set)
+
+numbers_set = set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(numbers_set)
+
+string_set = set(["a", "b", "c", "d", "e", "f", "g"])
+print(string_set)
+
+hello_set = set("hello")
+print(hello_set)
+
+hello_list = list("hello")
+print(hello_list)
